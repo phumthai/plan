9b16f96f1ca30ec471ccc1d9615ed819f91097d6
phumthai <phumthai.spy@hotmail.com>
2021-03-22 06:34:32 +0700

    auth fix

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ const { response } = require('express')
 const clientID = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 const redirectUri = process.env.CALLBACK_URL
-
+var access_token = "";
+var refresh_token = "";
 // Declare the callback route
 app.get('/auth', (req, res) =>{
     res.redirect(`https://oauth.cmu.ac.th/v1/Authorize.aspx?response_type=code&client_id=${clientID}&redirect_uri=${redirectUri}&scope=cmuitaccount.basicinfo&state=xyz`)
@@ -49,19 +50,21 @@ app.get('/callback', (req,res) => {
     }).then((resp) => {
         access_token = resp.data.access_token
         refresh_token = resp.data.refresh_token
-        axios({
-            method: 'get',
-            url: `https://misapi.cmu.ac.th/cmuitaccount/v1/api/cmuitaccount/basicinfo`,
-            headers: {
-                Authorization: 'Bearer ' + access_token
-            }
-        }).then((respd) => {
-            res.render('pages/staffs',{ userData: respd.data });
-        })
+        res.redirect('/success');
     })
-
 })
 
+app.get('/success', (req,res) => {
+    axios({
+        method: 'get',
+        url: `https://misapi.cmu.ac.th/cmuitaccount/v1/api/cmuitaccount/basicinfo`,
+        headers: {
+            Authorization: 'Bearer ' + access_token
+        }
+    }).then((respd) => {
+        res.render('pages/staffs',{ userData: respd.data });
+    })
+})
 
 
 
